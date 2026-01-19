8d71455832b3589168c407d5cc6a19209e67e714
Vedran Burojevic <vedran.burojevic@gmail.com>
2026-01-19 21:54:42 +0100

    Update instapaper-cli to v0.2.5

--- Formula/instapaper-cli.rb
+++ Formula/instapaper-cli.rb
@@ -4,8 +4,8 @@
 class InstapaperCli < Formula
   desc "Dependency-free Instapaper CLI"
   homepage "https://github.com/vburojevic/instapaper-cli"
-  url "https://github.com/vburojevic/instapaper-cli/archive/refs/tags/v0.2.3.tar.gz"
-  sha256 "4a124f3bdeb4db19ab0fc50ac2ffe3598def084f1b5c0654091d7024da7377f1"
+  url "https://github.com/vburojevic/instapaper-cli/archive/refs/tags/v0.2.5.tar.gz"
+  sha256 "68dcd392fee08378c0a4cab2ca144e0d05c759f4ddbf767f1968d5b03e29b42a"
   license "MIT"
 
   depends_on "go" => :build
