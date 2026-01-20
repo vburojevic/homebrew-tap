c0c38ac6dee97a7cabf2ffb94bf743bb68d62cf1
Vedran Burojević <vedran.burojevic@gmail.com>
2026-01-20 15:37:25 +0100

    chore: bump streaks-cli to v0.3.2

--- Formula/streaks-cli.rb
+++ Formula/streaks-cli.rb
@@ -1,18 +1,18 @@
 class StreaksCli < Formula
   desc "CLI for Streaks (Crunchy Bagel)"
   homepage "https://github.com/vburojevic/streaks-cli"
-  url "https://github.com/vburojevic/streaks-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "1783aa1d7040b9b12fceb2d91a08241f559f695e8cafebcdaf1fe9b1403be506"
+  url "https://github.com/vburojevic/streaks-cli/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "685e5722110722994952d4c38db4bdb9c17d6addea8a8665cd94d38fc27f8367"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     ldflags = "-s -w -X streaks-cli/internal/cli.version=#{version}"
-    system "go", "build", "-ldflags", ldflags, "-o", bin/"streaks-cli", "./cmd/streaks-cli"
+    system "go", "build", "-ldflags", ldflags, "-o", bin/"st", "./cmd/streaks-cli"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/streaks-cli --version")
+    assert_match version.to_s, shell_output("#{bin}/st --version")
   end
 end
